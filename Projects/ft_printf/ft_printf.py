13262a765bd6f40470e3500a35c0e3c3cc662504
Leo Farhi <lfarhi@made-f0Cr6s6.clusters.42paris.fr>
2024-05-23 18:49:03 +0200

    updt

--- Projects/ft_printf/ft_printf.py
+++ Projects/ft_printf/ft_printf.py
@@ -17,12 +17,29 @@ class Exo(BaseExercise):
         print(Process(["make"],cwd=Config.temp_path))
         AutoMain(None, None,"""
 		#include <ft_printf.h>
+		#include <stdio.h>
+        
 		int main(void)
 		{
 			//ft_printf("", "Hello","yaa");
             //ft_putnbr_fd(42, 1);
-            ft_printf("Hello\\n");
-            ft_printf("Hello % %\\n", "World", "!");
+            //ft_printf("Hello\\n");
+            /*int res = ft_printf("Hello %s %% %c\\n", "World", '!');
+            if (res == -1)
+                printf("Error in ft_printf (-1)\\n");
+            else
+				printf("ft_printf returned %d\\n", res);
+            ft_printf("\\n", 0, 0);
+            char *str = "Hello";
+            ft_printf("# %p %p #\\n", 0, 0);
+            ft_printf("# %p %p #\\n", str, 0);
+            ft_printf("# %p %p #\\n", (void *)5645564564, 0);
+            printf("# %p %p #\\n", NULL, NULL);
+            printf("# %p %p #\\n", str, NULL);
+            printf("# %p %p #\\n", (void *)5645564564, NULL);*/
+            char *str = NULL;
+            ft_printf("%23s#\\n", str);
+            printf("%23s#\\n", str);
 			return (0);
 		}
 		""")
@@ -30,7 +47,7 @@ class Exo(BaseExercise):
         cmd = "gcc -Wall -Wextra -Werror -I./includes -o Output main.c -L. -lftprintf"
         print(cmd)
         result = Process(cmd,stderr=True,shell=True,cwd=Config.temp_path)
-        print(result)
+        print(result.stdout+"\n"+result.stderr)
         return 'Output' in os.listdir(Config.temp_path)
 
     def Execute(self):
