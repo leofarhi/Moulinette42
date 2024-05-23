2093738fad142205465406e29e9244bc7c8d397c
Leo Farhi <lfarhi@made-f0Cr6s6.clusters.42paris.fr>
2024-05-23 14:35:09 +0200

    test printf

--- Projects/ft_printf/ft_printf.py
+++ Projects/ft_printf/ft_printf.py
@@ -14,15 +14,24 @@ class Exo(BaseExercise):
         self.path = Config.project_path
 
     def Compile(self):
+        print(Process(["make"],cwd=Config.temp_path))
         AutoMain(None, None,"""
-#include "ft_printf.h"
-int main(void)
-{
-    ft_printf("", "Hello","yaa");
-    return (0);
-}
-""")
-        return CompileTemp()
+		#include <ft_printf.h>
+		int main(void)
+		{
+			//ft_printf("", "Hello","yaa");
+            //ft_putnbr_fd(42, 1);
+            ft_printf("Hello\\n");
+            ft_printf("Hello % %\\n", "World", "!");
+			return (0);
+		}
+		""")
+        #add libftprintf.a to the compilation
+        cmd = "gcc -Wall -Wextra -Werror -I./includes -o Output main.c -L. -lftprintf"
+        print(cmd)
+        result = Process(cmd,stderr=True,shell=True,cwd=Config.temp_path)
+        print(result)
+        return 'Output' in os.listdir(Config.temp_path)
 
     def Execute(self):
         return ExecuteCode()==0
